b1c63288797abbe93959d02fe7b75ee4734d3799
Barboza00-cyber <consultor.barboza00@gmail.com>
2026-01-12 16:52:10 -0300

    Add age verification script in Python

---
+++ verificador_idade.py
@@ -0,0 +1,9 @@
+Python
+
+# Meu primeiro script de estudo em Python
+idade = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print("Você já é maior de idade!")
+else:
+    print("Você ainda é menor de idade.")
