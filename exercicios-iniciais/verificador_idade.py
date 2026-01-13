081d3fabc5ffaa215dd1ea9c3008818deab4cb60
Barboza00-cyber <consultor.barboza00@gmail.com>
2026-01-12 22:03:45 -0300

    Update verificador_idade.py

--- exercicios-iniciais/verificador_idade.py
+++ exercicios-iniciais/verificador_idade.py
@@ -1,11 +1,11 @@
 **Python**
 
-# Meu primeiro script de estudo em Python
+#Meu primeiro script de estudo em Python
 
-## Solicita a idade do usuário e converte para número inteiro
+##Solicita a idade do usuário e converte para número inteiro
 idade = int(input("Digite sua idade: "))
 
-## Verifica se a idade é maior ou igual a 18 para permitir o acesso
+##Verifica se a idade é maior ou igual a 18 para permitir o acesso
 if idade >= 18:
     print("Você já é maior de idade!")
 else:
